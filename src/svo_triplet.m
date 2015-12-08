2b22eb37988a9a6ecdeb1018d08a805b86a8c4f2
Sumit Bindal <sbindal@cs.stonybrook.edu>
2015-12-08 07:11:58 -0500

    Added latest experiment results; code cleanup

--- src/svo_triplet.m
+++ src/svo_triplet.m
@@ -11,18 +11,7 @@ tuple_data = sortrows(tuple_data,[2]);
 
 % Compare video ids
 if ~strcmp(char(tuple_data{:,2}), char(temp.Var1))
-    % 
     tuple_data = [tuple_data temp.Var2];
     T2 = cell2table(tuple_data,'VariableNames',{'VideoNum','VideoID','Subject','Object','Verb'});
     writetable(T2,sub_ver_obj_file,'Delimiter','\t','WriteVariableNames',false);
 end
-%{
-fileID = fopen(verb_file,'r');
-[video_id,verb] = fscanf(fileID, ['%s: %s']);
-fclose(fileID);
-fileID = fopen(sub_ver_obj_file,'w');
-fprintf(fileID,'%6s %12s %12s %12s\n',[tuple_data{:,1}],[tuple_data{:,3}],verb,[tuple_data{:,4}]);
-fclose(fileID)
-%}
-% [tuple_data{:,2}];
-% [video_id];
